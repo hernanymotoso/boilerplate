efb16b195f47c6a68980f0a0f9b3221103d32fc8
Hernany Motoso <hernanymotoso@gmail.com>
2023-08-05 12:32:08 -0300

    chore: add and config styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import * as S from './App.styles'
 
 function App() {
   return (
-    <></>
-
-  );
+    <S.Wrapper>
+      <p>teste</p>
+    </S.Wrapper>
+  )
 }
 
-export default App;
+export default App
