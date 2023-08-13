1e62a4f0cf66ad837ff0e38ecf72104402680464
Hernany Motoso <hernanymotoso@gmail.com>
2023-08-13 18:03:37 -0300

    Revert "feat: create line loading animation"
    This reverts commit babe9d51ad685ed2b23af81e2c12ed9f9b395557.

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,15 +1,4 @@
-import styled, { keyframes } from 'styled-components'
-
-const lineMove = keyframes`
-  0%, 100% {
-    transform: translateX(-100%)
-  }
-  50% {
-    transform: translateX(100%)
-  }
-
-
-`
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
@@ -18,25 +7,4 @@ export const Wrapper = styled.div`
 
   width: 100%;
   height: 100vh;
-  background: #3c3d48;
-
-  .loader-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
-
-  .loader {
-    width: 80px;
-    height: 30px;
-    position: relative;
-  }
-
-  .line {
-    height: 3px;
-    border-radius: 3px;
-    background-color: #5474e7;
-    animation: ${lineMove} 1.5s ease-in-out infinite;
-  }
 `
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import * as S from './App.styles'
 function App() {
   return (
     <S.Wrapper>
-      <div className="loader-container">
-        <div className="loader">
-          <div className="line"></div>
-        </div>
-      </div>
+      <p>teste</p>
     </S.Wrapper>
   )
 }
